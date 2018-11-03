df021633cfb45f35d12af418be2af4cfb0951789
Joseph Corona <josed.hc96@gmail.com>
2018-11-03 18:23:46 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,6 +11,10 @@ def display_board(board)
   puts " #{board[6]}" | " #{board[7]}" | " #{board[8]} "
 end
 
+def input_to_index(input)
+  input.strip.to_i
+end
+
 def valid_move?(board, index)
 
   if 0 <= index && index < 9 then
